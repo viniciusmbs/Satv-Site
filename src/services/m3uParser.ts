13b97553731cccc93b008a763e36331fc6fc1c6a
viniciusmbs <viniciusmbs@gmail.com>
2026-09-10 13:31:48 -0300

    Update m3uParser.ts

--- src/services/m3uParser.ts
+++ src/services/m3uParser.ts
@@ -161,7 +161,7 @@ export const normalizeCategory = (rawGroup: string, name: string): string => {
     upperName.includes('AGRO') ||
     upperName.includes('FUTURA')
   ) {
-    return 'ABERTA';
+    return 'CANAIS';
   }
 
   return rawGroup.replace(/^CANAIS:\s*/i, '').trim() || 'GERAL';
