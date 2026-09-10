cf339fa1ef4f106720de02ad74632f9f5af544de
viniciusmbs <viniciusmbs@gmail.com>
2026-09-10 13:27:43 -0300

    Change return value for specific channel names

--- src/services/m3uParser.ts
+++ src/services/m3uParser.ts
@@ -161,7 +161,7 @@ export const normalizeCategory = (rawGroup: string, name: string): string => {
     upperName.includes('AGRO') ||
     upperName.includes('FUTURA')
   ) {
-    return 'GERAL';
+    return 'TV ABERTA';
   }
 
   return rawGroup.replace(/^CANAIS:\s*/i, '').trim() || 'GERAL';
