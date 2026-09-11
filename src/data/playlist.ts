63f6e14c3c3c1971abc0ecc7b989b1fe9da1ce8e
viniciusmbs <viniciusmbs@gmail.com>
2026-09-10 22:23:33 -0300

    Modify Multishow URL in m3uPlaylist
    Updated the m3uPlaylist with new Multishow URL.

--- src/data/playlist.ts
+++ src/data/playlist.ts
@@ -2,7 +2,7 @@
 export const m3uPlaylist = `#EXTM3U
 
 #EXTINF:-1 group-title="VARIEDADES",Multishow
-https://rdcanais.net/multishowalternativo
+https://v1.rdse.buzz/multishow
 
 #EXTINF:-1 group-title="VARIEDADES",Viva
 https://v1.rdse.buzz/globoplaynovelas
